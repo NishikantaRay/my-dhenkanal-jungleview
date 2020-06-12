0abed602ba62b5f43029f122fb8b866120e4ff29
Nishikanta Ray <62615392+NishikantaRay@users.noreply.github.com>
2020-06-13 00:56:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ function msg(){
     alert("JUNGLEVIEW THANKING YOU!");
 
 }
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    var y= document.getElementById("panel");
+    if(y.style.display === "none"){
+        y.style.display="block";
+    } else{
+        y.style.display= "none";
+    }
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }  
+}
+function my(){
+    var div= document.getElementById("newpost");
+    if(div.styleSheets.display === "none")
+    {
+        div.styleSheets.display="block";
+
+    }
+    else{
+        div.style.display ="none";
+    }
+}
